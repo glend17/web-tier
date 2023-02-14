74c16696af9f2572414e1979d33d74b0c8238db4
glend17 <glendsouza1702@gmail.com>
2023-02-13 19:09:11 -0700

    Update WebController.java

--- src/main/java/org/cse546/controller/WebController.java
+++ src/main/java/org/cse546/controller/WebController.java
@@ -2,6 +2,8 @@ package org.cse546.controller;
 
 import org.cse546.services.S3Service;
 import org.cse546.services.SQSService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
@@ -11,6 +13,8 @@ import java.util.List;
 @RestController
 @RequestMapping
 public class WebController {
+
+    private static final Logger logger = LoggerFactory.getLogger(WebController.class);
 
     @Autowired
     private S3Service s3Service;
@@ -25,7 +29,7 @@ public class WebController {
 
     @PostMapping("/uploadImagesAndGetResults")
     public String uploadImagesAndGetResults(@RequestParam("files") MultipartFile[] files) {
-
+        logger.info("in upload image and get results")
         //processes the input and waits and fetches all the responses
         List<String> fileNameList = s3Service.saveImagesToS3(files);
         //saved images are sent to SQS request queue
