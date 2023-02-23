e0cb0448e8b8ec828d691bd2eb664a5c3f3d0cf8
Pratyush Pandey <68020102+pratyushpandey3897@users.noreply.github.com>
2023-02-23 00:41:40 -0700

    Update WebController.java

--- src/main/java/org/cse546/controller/WebController.java
+++ src/main/java/org/cse546/controller/WebController.java
@@ -31,6 +31,7 @@ public class WebController {
     @PostMapping("/uploadImagesAndGetResults")
     public String uploadImagesAndGetResults(@RequestParam("myfile") MultipartFile files) throws InterruptedException {
         logger.info("in upload image and get results");
+
         MultipartFile[] files_arr = new MultipartFile[1];
         files_arr[0] = files;
         String image_name = files.getOriginalFilename ();
@@ -39,12 +40,21 @@ public class WebController {
         //saved images are sent to SQS request queue
         sqsService.sendSavedImagesToRequestQueue(fileNameList);
         Map<String,String> result = sqsService.receiveSQSResponse(fileNameList, image_name);
-        String res = null;
+        String finalResult = new String();
+
+
         for (Map.Entry< String, String > m: result.entrySet ()){
-            String file = m.getKey ();
-            res = m.getValue ();
+            String res = m.getValue();
+            String[] parts = res.split(",");
+            String k = parts[0];
+            String v = parts[1];
+
+            // Format the value string using placeholders
+            String formattedValue = String.format("{%s}: {%s}", k, v);
+
+            finalResult = formattedValue;
         }
-        return res ;
+        return finalResult ;
     }
 
 
